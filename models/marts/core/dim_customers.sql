a97d7528deaf116c52f32421602f7d791c0a8b5d
Dave Masino <davemasino@hotmail.com>
2023-02-06 01:15:42 +0000

    Fixes after the dbt Learn solution page

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -3,11 +3,7 @@ with customers as (
 ),
 
 orders as (
-    select * from {{ ref('stg_orders') }}
-),
-
-payments as (
-    select * from {{ ref('stg_payments')}}
+    select * from {{ ref('fct_orders') }}
 ),
 
 customer_orders as (
@@ -15,22 +11,13 @@ customer_orders as (
         customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-    from orders
-    group by 1
-),
-
-customer_lifetime_value as (
-    select 
-        customer_id,
+        count(order_id) as number_of_orders,
         sum(amount) as lifetime_value
     from orders
-    left join payments using (order_id)
     group by 1
 ),
 
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
@@ -38,11 +25,9 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        coalesce(customer_lifetime_value.lifetime_value, 0) as lifetime_value
+        customer_orders.lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
-    left join customer_lifetime_value using (customer_id)
 )
 
 select * from final
