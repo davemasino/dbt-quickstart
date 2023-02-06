5f33df2753eb22e3794b3e1610e93db72fa00b05
Dave Masino <davemasino@hotmail.com>
2023-02-06 00:58:00 +0000

    Add clv to dim customers

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,30 +1,33 @@
 with customers as (
-
     select * from {{ ref('stg_customers')}}
-
 ),
 
 orders as (
-
     select * from {{ ref('stg_orders') }}
+),
 
+payments as (
+    select * from {{ ref('stg_payments')}}
 ),
 
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
     from orders
-
     group by 1
-
 ),
 
+customer_lifetime_value as (
+    select 
+        customer_id,
+        sum(amount) as lifetime_value
+    from orders
+    left join payments using (order_id)
+    group by 1
+),
 
 final as (
 
@@ -34,12 +37,12 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_lifetime_value.lifetime_value, 0) as lifetime_value
     from customers
 
     left join customer_orders using (customer_id)
-
+    left join customer_lifetime_value using (customer_id)
 )
 
 select * from final
